9471d797c042f8c417a36dc4f5e5a3bf90c66d6c
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-04-17 17:22:26 +0930

    Add implementation

---
+++ map.go
@@ -0,0 +1,181 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+// Package ctmap implements a constant-time key-value map.
+package ctmap
+
+import "crypto/subtle"
+
+// Map is a constant-time key-value map.
+type Map struct {
+	m [][]byte
+
+	keySize, valSize int
+}
+
+// New returns a new constant-time map with the
+// given key and value sizes.
+//
+// Every key and value must be of equal size.
+//
+// For a constant-time equivalent of map[string]struct{},
+// use 0 for valSize.
+func New(keySize, valSize int) *Map {
+	return &Map{
+		keySize: keySize,
+		valSize: valSize,
+	}
+}
+
+// NewWithCapacity returns a new constant-time map with
+// the given key and value sizes. It preallocates the
+// map's backing storage, sized to fit capacity entries
+// without reallocating.
+//
+// Every key and value must be of equal size.
+//
+// For a constant-time equivalent of map[string]struct{},
+// use 0 for valSize.
+func NewWithCapacity(keySize, valSize, capacity int) *Map {
+	return &Map{
+		m: make([][]byte, 0, capacity),
+
+		keySize: keySize,
+		valSize: valSize,
+	}
+}
+
+// Add appends a new entry to the map. It does not check
+// for duplicates, nor does it handle them.
+func (m *Map) Add(key, val []byte) {
+	if len(key) != m.keySize {
+		panic("key has invalid size")
+	}
+
+	if len(val) != m.valSize {
+		panic("val has invalid size")
+	}
+
+	entry := make([]byte, m.keySize+m.valSize)
+	copy(entry[:m.keySize], key)
+	copy(entry[m.keySize:], val)
+
+	m.m = append(m.m, entry)
+}
+
+// Set sets an existing map entry to val in constant-time.
+// It returns 1 if the key was present and the val set, 0
+// otherwise.
+//
+// If there are multiple entries with the same key, each
+// will have it's value set to val.
+func (m *Map) Set(key, val []byte) (v int) {
+	if len(key) != m.keySize {
+		panic("key has invalid size")
+	}
+
+	if len(val) != m.valSize {
+		panic("val has invalid size")
+	}
+
+	for _, entry := range m.m {
+		vv := subtle.ConstantTimeCompare(entry[:m.keySize], key)
+		subtle.ConstantTimeCopy(vv, entry[m.keySize:], val)
+		v |= vv
+	}
+
+	return
+}
+
+// Replace replaces an existing entry in the map with a new
+// entry in constant-time. It returns 1 if oldKey was present
+// in the map and the entry replaced, 0 otherwise.
+//
+// If there are duplicate entries matching oldKey, only the
+// first entry will be replaced.
+func (m *Map) Replace(oldKey, newKey, val []byte) (v int) {
+	if len(oldKey) != m.keySize {
+		panic("oldKey has invalid size")
+	}
+
+	if len(newKey) != m.keySize {
+		panic("newKey has invalid size")
+	}
+
+	if len(val) != m.valSize {
+		panic("val has invalid size")
+	}
+
+	for _, entry := range m.m {
+		vv := subtle.ConstantTimeCompare(entry[:m.keySize], oldKey) &^ v
+		subtle.ConstantTimeCopy(vv, entry[:m.keySize], newKey)
+		subtle.ConstantTimeCopy(vv, entry[m.keySize:], val)
+		v |= vv
+	}
+
+	return
+}
+
+// ReplaceKey is like Replace but it leaves the value untouched.
+//
+// If there are duplicate entries matching oldKey, only the
+// first entry's key will be replaced.
+func (m *Map) ReplaceKey(oldKey, newKey []byte) (v int) {
+	if len(oldKey) != m.keySize {
+		panic("oldKey has invalid size")
+	}
+
+	if len(newKey) != m.keySize {
+		panic("newKey has invalid size")
+	}
+
+	for _, entry := range m.m {
+		vv := subtle.ConstantTimeCompare(entry[:m.keySize], oldKey) &^ v
+		subtle.ConstantTimeCopy(vv, entry[:m.keySize], newKey)
+		v |= vv
+	}
+
+	return
+}
+
+// Contains determines if a key is present in the map in
+// constant-time. It returns 1 if the key is present, 0
+// otherwise.
+func (m *Map) Contains(key []byte) (v int) {
+	if len(key) != m.keySize {
+		panic("key has invalid size")
+	}
+
+	for _, entry := range m.m {
+		v |= subtle.ConstantTimeCompare(entry[:m.keySize], key)
+	}
+
+	return
+}
+
+// Lookup finds the value associated with a key in
+// constant-time. The value is copied, in constant-time,
+// into val which must be the correct length. It returns
+// 1 if the key was present, 0 otherwise.
+//
+// If there are multiple entries matching key, only the
+// first will be returned.
+func (m *Map) Lookup(key, val []byte) (v int) {
+	if len(key) != m.keySize {
+		panic("key has invalid size")
+	}
+
+	if len(val) != m.valSize {
+		panic("val has invalid size")
+	}
+
+	for _, entry := range m.m {
+		vv := subtle.ConstantTimeCompare(entry[:m.keySize], key) &^ v
+		subtle.ConstantTimeCopy(vv, val, entry[m.keySize:])
+		v |= vv
+	}
+
+	return
+}
---
+++ map_test.go
@@ -0,0 +1,138 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+package ctmap
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	for _, c := range []struct{ before, after [][]byte }{
+		{nil, [][]byte{{0xa5, 0x5a}}},
+		{[][]byte{{0, 0}}, [][]byte{{0, 0}, {0xa5, 0x5a}}},
+		{[][]byte{{0xa5, 0x5a}}, [][]byte{{0xa5, 0x5a}, {0xa5, 0x5a}}},
+	} {
+		m := &Map{m: c.before, keySize: 1, valSize: 1}
+		m.Add([]byte{0xa5}, []byte{0x5a})
+
+		if !reflect.DeepEqual(m.m, c.after) {
+			t.Error("Add failed")
+			t.Logf("expected: %02x", c.after)
+			t.Logf("got:      %02x", m.m)
+			t.Fatal()
+		}
+	}
+}
+
+func TestSet(t *testing.T) {
+	for _, c := range []struct{ before, after [][]byte }{
+		{nil, nil},
+		{[][]byte{{0, 0}}, [][]byte{{0, 0}}},
+		{[][]byte{{0xa5, 0x5a}}, [][]byte{{0xa5, 0xff}}},
+		{[][]byte{{0, 0}, {0xa5, 0x5a}, {0, 0}}, [][]byte{{0, 0}, {0xa5, 0xff}, {0, 0}}},
+		{[][]byte{{0xa5, 0x5a}, {0xa5, 0x5a}}, [][]byte{{0xa5, 0xff}, {0xa5, 0xff}}},
+	} {
+		m := &Map{m: c.before, keySize: 1, valSize: 1}
+		m.Set([]byte{0xa5}, []byte{0xff})
+
+		if !reflect.DeepEqual(m.m, c.after) {
+			t.Error("Set failed")
+			t.Logf("expected: %02x", c.after)
+			t.Logf("got:      %02x", m.m)
+			t.Fatal()
+		}
+	}
+}
+
+func TestReplace(t *testing.T) {
+	for _, c := range []struct{ before, after [][]byte }{
+		{nil, nil},
+		{[][]byte{{0, 0}}, [][]byte{{0, 0}}},
+		{[][]byte{{0xa5, 0x5a}}, [][]byte{{0x5a, 0xff}}},
+		{[][]byte{{0, 0}, {0xa5, 0x5a}, {0, 0}}, [][]byte{{0, 0}, {0x5a, 0xff}, {0, 0}}},
+		{[][]byte{{0xa5, 0x5a}, {0xa5, 0x5a}}, [][]byte{{0x5a, 0xff}, {0xa5, 0x5a}}},
+	} {
+		m := &Map{m: c.before, keySize: 1, valSize: 1}
+		m.Replace([]byte{0xa5}, []byte{0x5a}, []byte{0xff})
+
+		if !reflect.DeepEqual(m.m, c.after) {
+			t.Error("Replace failed")
+			t.Logf("expected: %02x", c.after)
+			t.Logf("got:      %02x", m.m)
+			t.Fatal()
+		}
+	}
+}
+
+func TestReplaceKey(t *testing.T) {
+	for _, c := range []struct{ before, after [][]byte }{
+		{nil, nil},
+		{[][]byte{{0, 0}}, [][]byte{{0, 0}}},
+		{[][]byte{{0xa5, 0x5a}}, [][]byte{{0x5a, 0x5a}}},
+		{[][]byte{{0, 0}, {0xa5, 0x5a}, {0, 0}}, [][]byte{{0, 0}, {0x5a, 0x5a}, {0, 0}}},
+		{[][]byte{{0xa5, 0x5a}, {0xa5, 0x5a}}, [][]byte{{0x5a, 0x5a}, {0xa5, 0x5a}}},
+	} {
+		m := &Map{m: c.before, keySize: 1, valSize: 1}
+		m.ReplaceKey([]byte{0xa5}, []byte{0x5a})
+
+		if !reflect.DeepEqual(m.m, c.after) {
+			t.Error("ReplaceKey failed")
+			t.Logf("expected: %02x", c.after)
+			t.Logf("got:      %02x", m.m)
+			t.Fatal()
+		}
+	}
+}
+
+func TestContains(t *testing.T) {
+	for _, c := range []struct {
+		m [][]byte
+		v int
+	}{
+		{nil, 0},
+		{[][]byte{{0, 0}}, 0},
+		{[][]byte{{0xa5, 0x5a}}, 1},
+		{[][]byte{{0, 0}, {0xa5, 0x5a}, {0, 0}}, 1},
+		{[][]byte{{0xa5, 0x5a}, {0xa5, 0x5a}}, 1},
+	} {
+		m := &Map{m: c.m, keySize: 1, valSize: 1}
+		v := m.Contains([]byte{0xa5})
+
+		if v != c.v {
+			t.Error("Contains failed")
+			t.Logf("expected: %d", c.v)
+			t.Logf("got:      %d", v)
+			t.Fatal()
+		}
+	}
+}
+
+func TestLookup(t *testing.T) {
+	for _, c := range []struct {
+		m   [][]byte
+		v   int
+		val []byte
+	}{
+		{nil, 0, []byte{0}},
+		{[][]byte{{0, 0}}, 0, []byte{0}},
+		{[][]byte{{0xa5, 0x5a}}, 1, []byte{0x5a}},
+		{[][]byte{{0, 0}, {0xa5, 0x5a}, {0, 0}}, 1, []byte{0x5a}},
+		{[][]byte{{0xa5, 0x11}, {0xa5, 0x22}}, 1, []byte{0x11}},
+	} {
+		m := &Map{m: c.m, keySize: 1, valSize: 1}
+
+		var val [1]byte
+		v := m.Lookup([]byte{0xa5}, val[:])
+
+		if v != c.v || !reflect.DeepEqual(val[:], c.val) {
+			t.Error("Lookup failed")
+			t.Logf("expected: 0x%02x, %d", c.val, c.v)
+			t.Logf("got:      0x%02x, %d", val[:], v)
+			t.Fatal()
+		}
+	}
+}
